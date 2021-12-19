9db74c95aa2e62b94e642bbf8480491bf88e8a8f
EL ouadi Abdelati <Elouadi_Abdelati@outlook.fr>
2021-12-19 20:44:47 +0100

    add gateway/hystrix/hystrix-dashboard

--- client-service/src/main/java/com/trading/clientservice/application/ws/ActionnaireRest.java
+++ client-service/src/main/java/com/trading/clientservice/application/ws/ActionnaireRest.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 
 @RestController
-@RequestMapping("/actionnaire")
+@RequestMapping("/actionnaires")
 @Tag(name = "actionnaire", description = "Cette classe permet de tester les process de la actionnaire")
 @Slf4j
 @RequiredArgsConstructor
---
+++ gateway/src/main/java/com/trading/action/gateway/GatewayApplication.java
@@ -0,0 +1,17 @@
+package com.trading.action.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableHystrix
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+
+}
